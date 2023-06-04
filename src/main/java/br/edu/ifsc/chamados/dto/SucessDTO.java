1207aa8ab5e4496acb385cb9ee083b27d6fc4eb8
Edder <egaddini.dev@gmail.com>
2023-06-03 23:47:26 -0300

    Ajustes no serviço/endpoint de registro de usuário.

---
+++ src/main/java/br/edu/ifsc/chamados/dto/SucessDTO.java
@@ -0,0 +1,14 @@
+package br.edu.ifsc.chamados.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SucessDTO {
+
+    private String mensagem;
+
+}
